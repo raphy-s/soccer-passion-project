57f80e08548d460664ee2136316787ce963e9210
raphy-s <raphaelsheh@gmail.com>
2026-06-10 09:21:12 -0400

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -23,7 +23,17 @@ st.caption(
 st.caption(
     f"Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 )
+# -----------------------
+# SUPABASE
+# -----------------------
 
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
 # -----------------------
 # SIDEBAR
 # -----------------------
